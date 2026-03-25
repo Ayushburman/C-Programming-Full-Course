86829b7e2075afd98ca875fa4d7869b7ccafe119
Ayush Burman <72551504+Ayushburman@users.noreply.github.com>
2026-03-25 23:12:13 +0530

    Update 06_ArrayStrings.c

--- 06_ArrayStrings.c
+++ 06_ArrayStrings.c
@@ -5,3 +5,18 @@
  * Compile : gcc array_basics.c -o array_basics
  * Run     : ./array_basics
  */
+int main(void) {
+
+    int arr[5] = {1, 2, 3, 4, 5};
+    int sum = 0;
+
+    printf("Array elements:\n");
+    for (int i = 0; i < 5; i++) {
+        printf("%d ", arr[i]);
+        sum += arr[i];
+    }
+
+    printf("\nSum = %d\n", sum);
+
+    return 0;
+}
