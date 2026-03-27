df5e45b5a209be8ca333175a6626df8a86141ed9
Ayush Burman <72551504+Ayushburman@users.noreply.github.com>
2026-03-27 20:03:27 +0530

    Update 01_DataTypes_Variables.c

--- 01_DataTypes_Variables.c
+++ 01_DataTypes_Variables.c
@@ -1,10 +1,8 @@
 /*
- * 07 — Data Types & Variables
+ * Data Types & Variables
  * ───────────────────────────────────────────────────────────────
  * Covers  : int, float, double, char, _Bool, short, long, unsigned
  *           sizeof, type limits, implicit & explicit casting
- * Compile : gcc data_types.c -o data_types
- * Run     : ./data_types
  */
 
 #include <stdio.h>
