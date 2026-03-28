414d6fa9667d29d809e1dd2868ede860ef5c73b8
Ayush Burman <72551504+Ayushburman@users.noreply.github.com>
2026-03-28 22:44:31 +0530

    Update 15.c

--- 15.c
+++ 15.c
@@ -4,3 +4,10 @@ int main() {
     printf("Hello, I am Ayush!\n");
     return 0;
 }
+----------------------------------
+// OUTPUT - Hello, I am Ayush!
+---------------------------------
+
+    
+
+    
