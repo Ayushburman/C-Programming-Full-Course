e8a4ef44b56481dd4102b936bcac42ee7c73688a
Ayush Burman <72551504+Ayushburman@users.noreply.github.com>
2026-03-28 22:47:54 +0530

    Update 15.c

--- 15.c
+++ 15.c
@@ -1,14 +1,25 @@
-// Hello UNIVERSE
+// 1
 #include <stdio.h>
 
 int main() {
     printf("Hello, I am Ayush!\n");
     return 0;
 }
-----------------------------------
+---------------------------------
 // OUTPUT - Hello, I am Ayush!
 ---------------------------------
+// 2
+#include <stdio.h>
 
+int main() {
+    int planet_COORD = 5972-12563-145263;
+    printf("planet_COORD: %d\n", planet_COORD);
+    return 0;
+}
 
+---------------------------------
+// OUTPUT - 5972-12563-145263
+---------------------------------
 
+//
     
