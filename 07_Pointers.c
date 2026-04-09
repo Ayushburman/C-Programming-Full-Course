a8283ca2e6cbba1ba6934fd05600e2beb1c0fcaf
Ayush Burman <72551504+Ayushburman@users.noreply.github.com>
2026-04-09 22:27:30 +0530

    Update 07_Pointers.c

--- 07_Pointers.c
+++ 07_Pointers.c
@@ -80,3 +80,10 @@ int main() {
     int *p = fun();
     printf("%d", *p);
 }
+
+#include <stdio.h>
+int main() {
+    int arr[3] = {1, 2, 3};
+    int (*p)[3] = &arr;
+    printf("%d", (*p)[1]);
+}
