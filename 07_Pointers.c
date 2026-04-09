422cd7e85a0876dfe726d8f7f8d18c0772a3222b
Ayush Burman <72551504+Ayushburman@users.noreply.github.com>
2026-04-09 22:23:18 +0530

    Update 07_Pointers.c

--- 07_Pointers.c
+++ 07_Pointers.c
@@ -22,16 +22,5 @@ int main() {
     int *p = &a;
     printf("%d", *p);
 }
-#include <stdio.h>
-int main() {
-    int a = 10;
-    int *p = &a;
-    printf("%d", *p);
-}
-#include <stdio.h>
-int main() {
-    int a = 10;
-    int *p = &a;
-    printf("%d", *p);
-}
+
 
