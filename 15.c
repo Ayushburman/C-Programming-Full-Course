519fb3c2472b3241da2ab2273ed67709a969da2a
Ayush Burman <72551504+Ayushburman@users.noreply.github.com>
2026-04-10 21:31:42 +0530

    Update 15.c

--- 15.c
+++ 15.c
@@ -312,9 +312,4 @@ int main(){
 -------------------------------
 ----------------------------------
 #include <stdio.h>
-
-int main(){
-    char name[20];
-    scanf("%s", name[20];
-    scanf("%s", name);
-    printf("%s\nkl
+in kn
